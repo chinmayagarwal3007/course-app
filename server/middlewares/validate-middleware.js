d4fcbc39826a6954eabc10f3638906a37864018c
Chinmay <agarwalchinmay3007@gmail.com>
2024-02-28 08:40:03 +0530

    error handling

--- server/middlewares/validate-middleware.js
+++ server/middlewares/validate-middleware.js
@@ -5,8 +5,16 @@ const validate = (schema) => async (req, res, next) => {
         req.body = parseBody;
         next();
     } catch (err) {
-        const message = err.errors[0].message;
-        res.status(400).json({msg: message});
+        const status = 422;
+        const message = "Fill the details properly"
+        const extraDetails = err.errors[0].message;
+        
+        const error = {
+            status,
+            message,
+            extraDetails
+        }
+        next(error);
     }
 }
 
--- server/server.js
+++ server/server.js
@@ -3,10 +3,13 @@ const express = require('express');
 const app = express();
 const router = require('./router/auth-router');
 const {connectDb} = require('./utils/db');
+const errorMiddleware = require("./middlewares/error-middleware");
 
 app.use(express.json());
 
 app.use("/api/auth", router); 
+
+app.use(errorMiddleware);
 
 const PORT = 5000;
 
