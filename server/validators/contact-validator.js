191a22b202d91d12a1f03aa32b98f8edf16bd4c3
Chinmay <agarwalchinmay3007@gmail.com>
2024-03-07 13:36:26 +0530

    some correction

--- server/validators/contact-validator.js
+++ server/validators/contact-validator.js
@@ -3,16 +3,18 @@ const {z} = require('zod');
 const ContactSchema = z.object({
     username: z
     .string({required_error: "UserName is required"})
-    .trim(),
+    .trim()
+    .min(1, "Username is required!"),
 
     email: z
     .string({required_error:"Email is required"})
     .trim()
-    .email({message: "Invalid email address"}),
+    .email({message: "Invalid email address!"}),
 
     message: z
     .string({required_error:"messsage is required"})
     .trim()
+    .min(1, "messsage is required!")
 
 })
 
